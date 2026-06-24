0ccabadcbfba1c835a775697eb303987191e850c
Paiva <159699832+PaivaK0ss@users.noreply.github.com>
2026-06-24 11:29:25 -0300

    Adição de static e Edit e Delete

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 import dados
 
 app = Flask(__name__)
 
 jogos = dados.carregar_do_arquivo()
+
+@app.route('/')
+def redirecionar_pagina():
+    return redirect('/jogos')
 
 @app.route('/jogos')
 def listar_jogos():
@@ -34,6 +38,35 @@ def criar_jogos():
         return redirect(listar_jogos())
     else:
         return render_template('form.html')
+
+@app.route('/jogos/alterar/<codigo>', methods=['GET', 'POST'])
+def alterar_jogo(codigo=None):
+    for i, jogo in enumerate(jogos):
+        if jogo['codigo'] == codigo:
+            if request.method == 'GET':
+                return render_template('edit.html', jogo=jogo)
+            jogo_atualizado = {
+                'codigo': request.form.get('codigo'),
+                'nome': request.form.get('nome'),
+                'criador': request.form.get('criador'),
+                'genero': request.form.get('genero'),
+                'tamanho': request.form.get('tamanho'),
+                'ano_lancamento': request.form.get('ano_lancamento'),
+                'plataforma': request.form.get('plataforma')
+            }
+            jogos[i] = jogo_atualizado
+            dados.salvar_no_arquivo(jogos)
+            return redirect('/jogos')
+    return redirect('/jogos')
+
+@app.route('/jogos/excluir/<codigo>', methods=['GET'])
+def excluir_jogo(codigo):
+    for i, jogo in enumerate(jogos):
+        if jogo['codigo'] == codigo:
+            del jogos[i]
+            break
+    dados.salvar_no_arquivo(jogos)
+    return redirect('/jogos')
 
 if __name__ == "__main__":
     app.run(debug=True)
